1d0d159a6a62c8fb787fd348e386dadbaaf7ac54
Chloe <chloe.chen@wsu.edu>
2026-02-21 14:07:54 -0800

    header and another c file

---
+++ CAMSCode/Header.h
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef HEADER_H
+#define HEADER_H
+
+
+
+#endif
--- CAMSCode/main.c
+++ CAMSCode/main.c
@@ -1,3 +1,4 @@
+#include "Header.h"
 
 int main(void) {
 
