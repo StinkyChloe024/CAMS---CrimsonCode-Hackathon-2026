c941aeecbca3aa8b5f9be031d9c7b58ced8aec31
Chloe <chloe.chen@wsu.edu>
2026-02-21 14:19:14 -0800

    fixed the name input lol

--- CAMSCode/main.c
+++ CAMSCode/main.c
@@ -2,12 +2,12 @@
 
 int main(void) 
 {
-	char name[15] = '\0';
+	char name[15] = { '\0' };
 
 	printf("Welcome to the Game of the Wheel!1\n");
 
 	printf("What is your name: ");
-	scanf("%s", name);
+	scanf("%s", &name);
 
 	printf("Welcome, %s, to the game!\n", name);
 
